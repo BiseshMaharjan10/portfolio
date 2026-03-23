f3b2145edbdace893391c338d9fc970774562630
BiseshMaharjan10 <biseshmhrz01@gmail.com>
2026-03-23 17:29:08 +0545

    Update CodeYatra project title

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -107,7 +107,7 @@ export const projects = [
     status: 'Production',
   },
   {
-    title: 'CodeYatra 2.0 - Async Avengers Stride',
+    title: 'CodeYatra 2.0 -  Stride',
     description:
       'Stride is an AI-powered productivity ecosystem that transforms career-building into a manageable daily routine. By combining psychometric discovery with dynamic, Trello-style roadmaps, Stride helps students navigate their future through progress forecasting and integrated burnout checks.',
     image: strideLogoImage,
